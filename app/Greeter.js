182f8b3cdd6ffcac94abc8486b60a33e2229409a
欧晓辉 <ouxh862@13322.com>
2017-11-25 17:39:45 +0800

    test

---
+++ app/Greeter.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/11/25.
+ */
+// Greeter.js
+module.exports = function() {
+    var greet = document.createElement('div');
+    greet.textContent = "Hi there and greetings!";
+    return greet;
+};
---
+++ app/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/11/25.
+ */
+//main.js
+const greeter = require('./Greeter.js');
+document.querySelector("#root").appendChild(greeter());
