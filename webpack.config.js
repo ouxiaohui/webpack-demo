588598df526653b0185983d232989671ba0992ea
欧晓辉 <ouxh862@13322.com>
2017-11-25 18:29:15 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 /**
  * Created by Administrator on 2017/11/25.
  */
+
 module.exports = {
+    devtool: 'source-map',
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
         path: __dirname + "/public",//打包后的文件存放的地方
