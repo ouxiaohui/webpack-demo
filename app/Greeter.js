c5d45790feb38007fe63e2133bb4a78bd220dcaa
ouxiaohui <627424435@qq.com>
2017-11-26 22:36:22 +0800

    babel

--- app/Greeter.js
+++ app/Greeter.js
@@ -2,8 +2,25 @@
  * Created by Administrator on 2017/11/25.
  */
 // Greeter.js
-module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
-    return greet;
-};
+// var config = require('./config.json');
+
+// module.exports = function() {
+//     var greet = document.createElement('div');
+//     greet.textContent = config.greetText;
+//     return greet;
+// };
+
+import config from './config.json';
+import React, {Component} from 'react';
+
+class Greeter extends Component{
+    render() {
+        return (
+            <div>
+                {config.greetText}
+            </div>
+        )
+    }
+}
+export default Greeter
+
--- app/main.js
+++ app/main.js
@@ -2,5 +2,12 @@
  * Created by Administrator on 2017/11/25.
  */
 //main.js
-const greeter = require('./Greeter.js');
-document.querySelector("#root").appendChild(greeter());
+// const greeter = require('./Greeter.js');
+// console.log(222);
+// document.querySelector("#root").appendChild(greeter());
+
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+render(<Greeter />, document.getElementById('root'));
