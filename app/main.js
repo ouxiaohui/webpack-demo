0d6d1b6b23c4bffbb5ce5be6dd0f083d65c7f076
欧晓辉 <ouxh862@13322.com>
2017-11-27 19:02:56 +0800

    test

--- app/main.js
+++ app/main.js
@@ -10,4 +10,4 @@ import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './Greeter';
 
-render(<Greeter />, document.getElementById('root'));
+render(<Greeter />, document.getElementById('root'))
